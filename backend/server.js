949d1b9012138eee9664700ccb12286a91097aae
djukic03 <djukic.nemanja003@gmail.com>
2024-08-24 22:37:18 +0200

    Database created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { readFile, writeFile } from "fs/promises";
+import mysql from "mysql2";
 
 const app = express();
 const port = 5000;
@@ -9,21 +9,94 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: "athlete-x-database",
+  user: "admin",
+  password: "admin",
+  database: "athlete_x_database",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+
+  const createUsersTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+      username VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+      fullName VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );`;
+
+  connection.query(createUsersTableQuery, (err, result) => {
+    if (err) throw err;
+    console.log("Users table created successfully");
+  });
+
+  const createWorkoutsTableQuery = `
+    CREATE TABLE IF NOT EXISTS workouts (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      exercise VARCHAR(255) NOT NULL,
+      reps INT NOT NULL,
+      sets INT NOT NULL,
+      weight DECIMAL(5, 2) NOT NULL
+    );`;
+
+  connection.query(createWorkoutsTableQuery, (err, result) => {
+    if (err) throw err;
+    console.log("Workouts table created successfully");
+  });
+
+  const createUserWorkoutsTableQuery = `
+    CREATE TABLE IF NOT EXISTS users_workouts (
+      username VARCHAR(255) NOT NULL,
+      date VARCHAR(255) NOT NULL,
+      workout_id INT NOT NULL,
+      PRIMARY KEY (username, workout_id),
+      FOREIGN KEY (username) REFERENCES users(username),
+      FOREIGN KEY (workout_id) REFERENCES workouts(id)
+    );`;
+
+  connection.query(createUserWorkoutsTableQuery, (err, result) => {
+    if (err) throw err;
+    console.log("User-Workouts table created successfully");
+  });
+
+  const insertIntoUsersTableQuery = `
+    INSERT INTO users (username, fullName, email, password) VALUES ("admin", "admin admin", "admin@admin.com", "admin")
+  `;
+  connection.query(insertIntoUsersTableQuery, (err, result) => {
+    if (err) throw err;
+    console.log("Admin user inserted successfully");
+  });
+});
+
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+/* ***********************************************************
+
+  ODAVDE NA DOLE NE VALJA
+
+************************************************************** */
+
 app.post("/api/register", async (req, res) => {
-  const userData = req.body;
+  const { username, fullName, email, password } = req.body;
 
   try {
-    const data = await readFile("../frontend/public/usersData.json", "utf8");
-    const users = JSON.parse(data);
-    users.push(userData);
-
-    await writeFile(
-      "../frontend/public/usersData.json",
-      JSON.stringify(users, null, 2)
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+      "INSERT INTO users (username, fullName, email, password) VALUES (?, ?, ?, ?)",
+      [username, fullName, email, password]
     );
+    connection.release();
     res.send("User registered successfully");
   } catch (err) {
-    console.error("Error handling file:", err);
+    console.error("Error handling database:", err);
     res.status(500).send("Server Error");
   }
 });
@@ -32,26 +105,20 @@ app.put("/api/updateUser", async (req, res) => {
   const { username, fullName, email } = req.body;
 
   try {
-    const data = await readFile("../frontend/public/usersData.json", "utf8");
-    const users = JSON.parse(data);
-    const userIndex = users.findIndex((user) => user.username === username);
-    if (userIndex === -1) {
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+      "UPDATE users SET fullName = ?, email = ? WHERE username = ?",
+      [fullName, email, username]
+    );
+    connection.release();
+
+    if (result.affectedRows === 0) {
       return res.status(404).send("User not found");
     }
 
-    users[userIndex] = {
-      ...users[userIndex],
-      fullName,
-      email,
-    };
-
-    await writeFile(
-      "../frontend/public/usersData.json",
-      JSON.stringify(users, null, 2)
-    );
     res.send("User updated successfully");
   } catch (err) {
-    console.error("Error handling file:", err);
+    console.error("Error handling database:", err);
     res.status(500).send("Server Error");
   }
 });
@@ -60,18 +127,29 @@ app.post("/api/saveWorkout", async (req, res) => {
   const { username, date, workoutData } = req.body;
 
   try {
-    const data = await readFile("./public/workouts.json", "utf8");
-    const workouts = JSON.parse(data);
-    const newWorkout = { username, date, workoutData };
-    workouts.push(newWorkout);
+    const connection = await pool.getConnection();
 
-    await writeFile(
-      "./public/workouts.json",
-      JSON.stringify(workouts, null, 2)
-    );
+    const workoutIds = [];
+    for (const workout of workoutData) {
+      const { exercise, reps, sets, weight } = workout;
+      const [result] = await connection.execute(
+        "INSERT INTO workouts (exercise, reps, sets, weight) VALUES (?, ?, ?, ?)",
+        [exercise, reps, sets, weight]
+      );
+      workoutIds.push(result.insertId);
+    }
+
+    for (const workoutId of workoutIds) {
+      await connection.execute(
+        "INSERT INTO users_workouts (username, date, workout_id) VALUES (?, ?, ?)",
+        [username, date, workoutId]
+      );
+    }
+
+    connection.release();
     res.send("Workout saved successfully");
   } catch (err) {
-    console.error("Error handling file:", err);
+    console.error("Error handling database:", err);
     res.status(500).send("Server Error");
   }
 });
--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,3 @@
 SET GLOBAL log_error_verbosity = 1;
 ALTER USER 'admin'@'%' IDENTIFIED WITH caching_sha2_password BY 'admin';
 GRANT ALL PRIVILEGES ON athlete_x_database.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
-EXIT;
